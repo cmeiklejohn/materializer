897deffa083968eef2ef8b0909e1b61106ec3a07
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2011-08-11 22:30:30 -0400

    Add guard, fix up database seeding.

--- materializer.gemspec
+++ materializer.gemspec
@@ -35,4 +35,11 @@ Gem::Specification.new do |s|
   s.add_development_dependency('simplecov') 
   s.add_development_dependency('diesel') 
   s.add_development_dependency('ZenTest') 
+  s.add_development_dependency('guard') 
+  s.add_development_dependency('guard-rspec')
+
+  if RUBY_PLATFORM =~ /linux/i
+    s.add_development_dependency('rb-inotify')
+    s.add_development_dependency('libnotify')
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,10 +17,23 @@ require 'timecop'
 
 require 'materializer'
 
-require 'simplecov'
-SimpleCov.start
+if RUBY_VERSION > "1.9"
+  require 'simplecov'
+  SimpleCov.start
+end
 
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3", 
+  :database => ":memory:"
+)
+
+ActiveRecord::Base.silence do
+  ActiveRecord::Migration.verbose = false
+
+  load(File.dirname(__FILE__) + '/../db/schema.rb')
+end
 
 RSpec.configure do |config|
   config.use_transactional_fixtures = true
